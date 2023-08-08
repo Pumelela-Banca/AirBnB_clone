5fdfd97c074b330b45a789297e46bdbbd86f8e8f
Julesntizimira <ntizimijules5@gmail.com>
2023-08-08 13:00:28 +0000

    3rd and 4th task

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+'''define class BaseModel'''
 import uuid
 import datetime
 
+
 class BaseModel:
+    '''class BaseModel'''
     def __init__(self, *args, **kwargs):
+        '''init Method - constructor'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
@@ -20,12 +23,27 @@ class BaseModel:
                     self.key = kwargs[key]
 
     def __str__(self):
+        ''' return printable string represantation
+            of an instance
+        '''
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        '''updates the public instance attribute updated_at
+           with the current datetime
+        '''
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        '''returns a dictionary containing all keys/values
+           of __dict__ of the instance:
+           * by using self.__dict__, only instance attributes set will be returned
+           * a key __class__ must be added to this dictionary with the
+           class name of the object created_at and updated_at must be
+           converted to string object in ISO format:
+           format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
+           you can use isoformat() of datetime object
+        '''
         dict1 = self.__dict__
         dict1['created_at'] = self.created_at.isoformat()
         dict1['updated_at'] = self.updated_at.isoformat()
