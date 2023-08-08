f4c473bef6adf423c647d636180a18a8568b19f2
Julesntizimira <ntizimijules5@gmail.com>
2023-08-08 16:46:30 +0000

    task 6

--- console.py
+++ console.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '$: '
+    
+    def do_help(self, line):
+        '''Get some help'''
+        if line == 'quit':
+            print('Quit command to exit the program')
+        elif line == 'EOF':
+            print('EOF command to exit the program')
+        else:
+            print('Documented commands (type help <topic>):')
+            print('========================================')
+            print('EOF  help  quit\n')
+
+    def do_quit(self, line):
+        '''exit the program'''
+        return True
+
+    def do_EOF(self, line):
+        '''exit the program'''
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
