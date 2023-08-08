6da974ce3c5518f82594f2ad13fdbbdbe2bfe2ef
Julesntizimira <ntizimijules5@gmail.com>
2023-08-08 18:10:54 +0000

    7th task create and show methods

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
+from models import base_model
 import cmd
+import os
+import json
 
 class HBNBCommand(cmd.Cmd):
     prompt = '$: '
@@ -23,6 +25,46 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         '''exit the program'''
         return True
+
+    def do_create(self, line):
+        ''' Creates a new instance of BaseModel '''
+        if line == '':
+            print('** class name missing **')
+        elif line == 'BaseModel':
+            model = base_model.BaseModel()
+            model.save()
+            print(model.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        ''' Prints the string representation of
+            an instance based on the class name
+        '''
+        if line == '':
+            print('** class name missing **')
+            return
+        args = line.split()
+        if args[0] != 'BaseModel':
+            print("** class doesn't exist **")
+            return 
+        elif len(args) == 1:
+            print('** instance id missing **')
+            return
+        else:
+            if os.path.exists('file.json'):
+                with open('file.json') as fh:
+                    obj = json.load(fh)
+                j = 0
+                for key in obj.keys():
+                    if args[1] in key:
+                        print(f"[{args[0]}] ({args[1]}) {obj[key]}")
+                        j = 1
+                if  j == 0:
+                    print('** no instance found **')
+            else:
+                print('** no instance found **')
+
 
 
 if __name__ == '__main__':
