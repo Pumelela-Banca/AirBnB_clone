0bade889a38f252ea4d44d7cab5a621ccd582b45
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-09 02:16:29 +0200

    added test file for base model with str test

---
+++ tests/test_base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Tests base model class and its instances
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test all possible inputs on BaseModel
+    """
+    def test_init(self):
+        """
+        test initialization of class
+        """
+        tes1 = BaseModel(name="Tom", number=89)
+        # attributes not added to dictionary __dict__
+        #self.assertTrue(isinstance(tes1.id, str))
+        #self.assertEqual(tes1.name, "Tom")
+        #self.assertEqual(tes1.number, 89)
+
+    def test_time(self):
+        """
+        test time values for created_at and updated_at
+        """
+        pass
+
+    def test_print_format(self):
+        """
+        tests __str__ result
+        """
+        test1 = BaseModel()
+        self.assertEqual(
+            f"[{test1.__class__.__name__}] ({test1.id}) {test1.__dict__}",
+            f"[BaseModel] ({test1.id}) {test1.__dict__}")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
