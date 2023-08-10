b0710a82cd765d2ad771d2431a57a3bd98b6a9c7
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-10 17:29:37 +0200

    added emty class to test city

---
+++ tests/test_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Tests base model class and its instances
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """
+    Test city class inputs
+    """
+    pass
