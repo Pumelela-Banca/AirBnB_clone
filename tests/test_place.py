1a49e60978750c0f400d52cb86be82478ae2add8
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-10 17:47:02 +0200

    added test class for place

---
+++ tests/test_place.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module to test place
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """
+    tests for place
+    """
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
