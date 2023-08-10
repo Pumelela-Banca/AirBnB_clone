d20916541138265e1ca62c1b33c2b090fbe90114
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-10 17:19:35 +0200

    added empty test file

---
+++ tests/test_amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Tests base model class and its instances
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test all possible inputs on Amenity
+    """
+    def test_creation(self):
+        """
+        Test if class can be developed
+        """
+        pass
