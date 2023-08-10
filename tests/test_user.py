4a7386604c3d80f0a1af77297b4c73b6199bfe5b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-10 18:21:30 +0200

    added test class for user

---
+++ tests/test_user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module with tests for User
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """
+    test all cases for state
+    """
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
