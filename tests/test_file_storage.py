1f4445f37b90e5865754429ca4aa19ff49661f03
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-12 20:18:26 +0200

    added test for id in __object

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -6,6 +6,7 @@ Tests file storage class and its instances
 
 
 import unittest
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
@@ -13,14 +14,26 @@ class TestFileStorage(unittest.TestCase):
     """
     tests for storage methods
     """
+    def setUp(self) -> None:
+        """
+        set up store
+        """
+        self.store = FileStorage()
 
     def test_all(self):
         """
         test cases for all method
         """
         # empty dictionary
-        store = FileStorage()
-        self.assertTrue(type(store.all()) is dict)
+        self.assertTrue(type(self.store.all()) is dict)
+
+    def test_new(self):
+        """
+        test if new works
+        """
+        tes1 = BaseModel(name="Jake", place="NT")
+        self.store.new(tes1)
+        self.assertIn("BaseModel." + tes1.id, self.store.all().keys())
 
 
 if __name__ == '__main__':
