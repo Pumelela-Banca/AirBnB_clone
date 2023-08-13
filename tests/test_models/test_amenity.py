8dd0c76374320e40f627558b20dfcfd2f6a442a6
Pumelela-Banca <pumelelabanca@gmail.com>
2023-08-13 14:27:52 +0200

    added str test in amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -18,12 +18,12 @@ class TestAmenity(unittest.TestCase):
         Test if class can be developed
         """
 
-        def test_print_format(self):
-            """
-            tests __str__ result
-            """
-            test1 = BaseModel()
-            self.assertEqual(str(test1), f"[Amenity] ({test1.id}) {test1.__dict__}")
+    def test_print_format(self):
+        """
+        tests __str__ result
+        """
+        test1 = BaseModel()
+        self.assertEqual(str(test1), f"[Amenity] ({test1.id}) {test1.__dict__}")
 
 
 if __name__ == '__main__':
